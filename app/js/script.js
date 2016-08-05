21ce2429f9eb934d80212774dd1178d0d051ae39
gorotto <yastreb92@inbox.ru>
2016-08-05 17:07:58 +0300

    add-page

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,11 @@ $('.p-deals__nav_btn').click(function(event) {
 	$('.p-deals__nav_btn').removeClass('p-deals__nav_btn-active');
 	$(this).addClass('p-deals__nav_btn-active');
 });
+
+$(function() {
+  $(".storey").click(function(e) {
+    e.preventDefault();
+    $(".storey").removeClass('active');
+    $(this).addClass('active');
+  })
+});
